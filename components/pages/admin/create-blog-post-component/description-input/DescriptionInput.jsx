0a64a8e6f71e885e095bfe004f60ecc63d3611eb
solovyovsdom <joelborofsky@mac.com>
2024-09-10 16:28:11 -0500

    updated the quill editor

--- components/pages/admin/create-blog-post-component/description-input/DescriptionInput.jsx
+++ components/pages/admin/create-blog-post-component/description-input/DescriptionInput.jsx
@@ -1,15 +1,29 @@
+'use client'
+import { useState } from 'react'
 import styles from './DescriptionInput.module.scss'
 
 function DescriptionInput({ value, onChange }) {
+  const [wordCount, setWordCount] = useState(0)
+
+  const handleInputChange = (e) => {
+    const words = e.target.value.split(/\s+/).filter(Boolean)
+    if (words.length <= 500) {
+      onChange(e)
+      setWordCount(words.length)
+    }
+  }
+
   return (
     <div className={styles.descriptionInput}>
-      <label htmlFor='description'> Description (Max 500 Words)</label>
+      <label htmlFor='description'>Description (Max 500 Words)</label>
       <textarea
         name='description'
         id='description'
         value={value}
-        onChange={onChange}
+        onChange={handleInputChange}
+        required
       ></textarea>
+      <div className={styles.wordCount}>{wordCount}/500 words</div>
     </div>
   )
 }
