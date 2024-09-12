6997f1b469e1f9fe0e54c96883227ea7631eca5a
solovyovsdom <joelborofsky@mac.com>
2024-09-11 20:39:50 -0500

    updated quill editor for deployment

--- components/pages/admin/create-blog-post-component/quill-editor/QuillEditor.jsx
+++ components/pages/admin/create-blog-post-component/quill-editor/QuillEditor.jsx
@@ -1,13 +1,30 @@
 'use client'
+import React, { useState, useEffect } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import styles from './QuillEditor.module.scss'
 
-const QuillEditor = ({ value, onChange }) => (
-  <div className={styles.quillEditor}>
-    <label htmlFor='content'>Content</label>
-    <ReactQuill className={styles.quillEditor__container} value={value} onChange={onChange} />
-  </div>
-)
+const QuillEditor = ({ value, onChange }) => {
+  const [isClient, setIsClient] = useState(false)
+
+  // Ensure this runs only on the client
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  // Only render ReactQuill on the client-side
+  return (
+    <div className={styles.quillEditor}>
+      <label htmlFor='content'>Content</label>
+      {isClient && (
+        <ReactQuill
+          className={styles.quillEditor__container}
+          value={value}
+          onChange={onChange}
+        />
+      )}
+    </div>
+  )
+}
 
 export default QuillEditor
